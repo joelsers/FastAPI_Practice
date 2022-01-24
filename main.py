390f7ac14e5e5df3246155a0168273f183507e2c
Joel <joeljacobs95@gmail.com>
2022-01-24 16:02:44 -0500

    created models file for pydantic strong typing. Adding routes for full crud

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
 import random
+from models import Item
 app = FastAPI()
+
+db = []
 
 
 @app.get("/")
 async def root():
     rand_num = random.randint(0,100)
     for x in range(1,100):
-        if rand_num == 1 or rand_num == 100:
+        if rand_num <= 20 or rand_num >= 80:
             return {"boom shocka locka": f'{rand_num}'}
         else:
             return {"message": f'{rand_num}'}
-    
+
+@app.get("/items")
+async def read_items():
+     return db
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
-    return {"item_id" : item_id}
+    return db[item_id]
+
+@app.post("/items")
+async def post_item(item: Item):
+    db.append(item.dict())
+    return db[-1]
+
+# @app.delete('/items/{item_id}')
+# async def delete_item(item_id: int):
+#     for item in db:
+#         if item['id'] == item_id:
+#             print(db.index(item))
+#             return db
+#         else:
+#             return {"no item with that id exists"}
